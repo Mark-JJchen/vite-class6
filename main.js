8a6681367276da26e72e347bcde4890eeab458cd
mark chen <dragon0721@livemail.tw>
2023-08-27 01:59:33 +0800

    fix modal v2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap';  //不寫前面它會自動去node_modules裡面找尋
-import "./node_modules/bootstrap/dist/js/bootstrap"
+// import 'bootstrap/dist/js/bootstrap';  //不寫前面它會自動去node_modules裡面找尋
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 // 跑馬燈動畫
 const swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto", // 自動計算每次滑動可容納個內容
