8d5f3bfe0894bbb40e94baf1e10c627b598f65ae
mark chen <dragon0721@livemail.tw>
2023-08-26 23:24:14 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap';  //不寫前面它會自動去node_modules裡面找尋
+
+// 跑馬燈動畫
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto", // 自動計算每次滑動可容納個內容
+    speed: 40000,
+    allowTouchMove: false, //避免滑鼠互動
+    loop: true, // 最後一個自動循環
+    autoplay: {
+        delay: 1
+        // disableOnInteraction 互動後輪播暫停預設為 false，不用再寫
+    }
+});
